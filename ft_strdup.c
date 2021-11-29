298dd5ae9167e17b08ada924db2a6607460a07a5
Maleaume BERQUER <mberquer@e2r2p8.clusters.42paris.fr>
2021-11-29 15:45:34 +0100

    yas

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mberquer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/29 14:42:34 by mberquer          #+#    #+#             */
-/*   Updated: 2021/11/29 15:20:08 by mberquer         ###   ########.fr       */
+/*   Created: 2021/11/29 15:22:35 by mberquer          #+#    #+#             */
+/*   Updated: 2021/11/29 15:35:09 by mberquer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void	*ft_calloc(size_t	nmemb, size_t	size)
+char	*strdup(const char	*s)
 {
-	int*	ptr;
+	char*	s2;
 
-	if ((nmemb * size) > 2147483647)
-		return error
-	if (nmemb && size)
-	{
-		ptr = malloc((nmemb * size));
-		ft_bzero(ptr, nmemb);
-		return ptr;
-	}
-	return NULL;
+	s2 = malloc(ft_strlen(s) + 1);
+	ft_strlcpy(s2, s, (ft_strlen(s) + 1));
+	return *s2;
 }
